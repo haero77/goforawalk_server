89fb608cf227d4c3057f5be82ef8068b8ef40f2d
haero77 <shineecard5@gmail.com>
2025-05-24 22:44:43 +0900

    [GW-56] 발자취는 하루 한 개 생성 제한에 대한 테스트 추가

--- backend/app-server/src/test/kotlin/side/flab/goforawalk/app/domain/footstep/application/FootstepServiceTest.kt
+++ backend/app-server/src/test/kotlin/side/flab/goforawalk/app/domain/footstep/application/FootstepServiceTest.kt
@@ -1,16 +1,19 @@
 package side.flab.goforawalk.app.domain.footstep.application
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.web.multipart.MultipartFile
 import side.flab.goforawalk.app.domain.footstep.application.dto.FootStepCreateRequest
+import side.flab.goforawalk.app.domain.footstep.domain.Footstep
 import side.flab.goforawalk.app.domain.footstep.domain.FootstepCreator
 import side.flab.goforawalk.app.domain.footstep.domain.FootstepImageNameGenerator
 import side.flab.goforawalk.app.domain.footstep.domain.FootstepReader
 import side.flab.goforawalk.app.domain.user.domain.UserReader
 import side.flab.goforawalk.app.support.BaseIntegrationTest
+import side.flab.goforawalk.app.support.fixture.FootstepFixture.save
 import side.flab.goforawalk.app.support.fixture.UserFixture.createSeoulUser
 import side.flab.goforawalk.app.support.fixture.UserFixture.save
 import side.flab.goforawalk.app.support.image.ImageUploader
@@ -31,18 +34,25 @@ class FootstepServiceTest : BaseIntegrationTest() {
     lateinit var imageNameGenerator: FootstepImageNameGenerator
 
     @Test
+    fun `유저가 오늘 이미 발자취를 생성한 경우, 발자취를 생성할 수 없다(하루 한 개 제한)`() {
+        val user = createSeoulUser(nickname = "산책왕").save(userRepository)
+        val footstepDate = LocalDate.of(2025, 5, 24)
+        Footstep(user, footstepDate, "imageUrl").save(footstepRepository)
+
+        val request = FootStepCreateRequest(user.id!!, mockMultipartFile(), "test-content")
+        val SUT = footstepService(clockHolder = FakeClockHolder(footstepDate))
+
+        // when, then
+        Assertions.assertThatThrownBy { SUT.createFootstep(request) }
+                    .isInstanceOf(IllegalStateException::class.java)
+    }
+
+    @Test
     fun `유저와 이미지 파일로 발자취를 생성할 수 있다`() {
         val user = createSeoulUser(nickname = "산책왕").save(userRepository)
-        val imageFile: MultipartFile = MockMultipartFile(
-            "image",
-            "image.jpg",
-            "image/jpeg",
-            "fake image content".toByteArray()
-        )
-
         val request = FootStepCreateRequest(
             userId = user.id!!,
-            imageFile = imageFile,
+            imageFile = multipartFile(),
             content = "test-content"
         )
 
@@ -65,8 +75,20 @@ class FootstepServiceTest : BaseIntegrationTest() {
         )
     }
 
+    private fun multipartFile(): MultipartFile {
+        val imageFile: MultipartFile = mockMultipartFile()
+        return imageFile
+    }
+
+    private fun mockMultipartFile() = MockMultipartFile(
+        "image",
+        "image.jpg",
+        "image/jpeg",
+        "fake image content".toByteArray()
+    )
+
     private fun footstepService(
-        imageUploader: ImageUploader,
+        imageUploader: ImageUploader = FakeImageUploader("fake-image-url"),
         clockHolder: ClockHolder
     ): FootstepService {
         return FootstepService(
