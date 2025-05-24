c022ded74f84cfced9e21073c64e771f51e78ebf
haero77 <shineecard5@gmail.com>
2025-05-25 02:05:33 +0900

    [GW-56] 발자취 조회, 삭제 컨트롤러 엔드포인트 구현

--- backend/app-server/src/main/kotlin/side/flab/goforawalk/app/api/footstep/FootstepController.kt
+++ backend/app-server/src/main/kotlin/side/flab/goforawalk/app/api/footstep/FootstepController.kt
@@ -1,10 +1,11 @@
 package side.flab.goforawalk.app.api.footstep
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import side.flab.goforawalk.app.api.footstep.dto.FootstepQueryResponse
 import side.flab.goforawalk.app.domain.footstep.application.FootstepCreateService
+import side.flab.goforawalk.app.domain.footstep.application.FootstepDeleteService
+import side.flab.goforawalk.app.domain.footstep.application.FootstepQueryService
 import side.flab.goforawalk.app.domain.footstep.application.dto.FootStepCreateRequest
 import side.flab.goforawalk.app.domain.footstep.application.dto.FootstepDetailResponse
 import side.flab.goforawalk.app.support.base.BaseApiController
@@ -12,14 +13,24 @@ import side.flab.goforawalk.app.support.response.ApiResponse
 
 @RestController
 class FootstepController(
-    private val footstepCreateService: FootstepCreateService
+    private val footstepCreateService: FootstepCreateService,
+    private val footstepQueryService: FootstepQueryService,
+    private val deleteService: FootstepDeleteService,
 ) : BaseApiController() {
+
+    @GetMapping("/v1/footsteps")
+    fun getFootsteps(
+        userId: Long, // todo MethodArgumentResolver를 통해 자동으로 주입
+    ): ApiResponse<FootstepQueryResponse> {
+        val footsteps = footstepQueryService.findAllFootStepsOfUser(userId)
+        return ApiResponse(FootstepQueryResponse(footsteps))
+    }
 
     @PostMapping("/v1/footsteps")
     fun createFootstep(
-        userId: Long, // MethodArgumentResolver를 통해 자동으로 주입
+        userId: Long, // todo MethodArgumentResolver를 통해 자동으로 주입
         @RequestPart("data") data: MultipartFile, // image file
-        @RequestPart("content") content: String?
+        @RequestPart("content") content: String?,
     ): ApiResponse<FootstepDetailResponse> {
         val request = FootStepCreateRequest(
             userId = userId,
@@ -28,5 +39,12 @@ class FootstepController(
         )
         val footstepDetailResponse = footstepCreateService.createFootstep(request)
         return ApiResponse(footstepDetailResponse)
+    }
+
+    @DeleteMapping("/v1/footsteps/{footstepId}")
+    fun deleteFootstep(
+        @PathVariable footstepId: Long,
+    ) {
+        deleteService.delete(footstepId)
     }
 }
