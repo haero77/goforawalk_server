1cb7036d81ec3dac3f66e24e5ab168695b83864e
haero77 <shineecard5@gmail.com>
2025-05-27 07:27:39 +0900

    [GW-56] CurrentUserId ArgumentResolver 추가

---
+++ backend/app-server/src/main/kotlin/side/flab/goforawalk/app/support/web/CurrentUserId.kt
@@ -0,0 +1,3 @@
+package side.flab.goforawalk.app.support.web
+
+annotation class CurrentUserId()
---
+++ backend/app-server/src/main/kotlin/side/flab/goforawalk/app/support/web/WebMvcConfig.kt
@@ -0,0 +1,15 @@
+package side.flab.goforawalk.app.support.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig(
+    private val currentUserIdArgumentResolver: CurrentUserIdArgumentResolver,
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(currentUserIdArgumentResolver)
+    }
+}
