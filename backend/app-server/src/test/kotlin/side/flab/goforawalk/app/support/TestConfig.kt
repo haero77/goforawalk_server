3590e820b607bb2e0e45e9867de468a3313c6189
haero77 <shineecard5@gmail.com>
2025-05-31 23:56:01 +0900

    [GW-56] E2E 테스트를 위한 BaseE2ETest 추가

--- backend/app-server/src/test/kotlin/side/flab/goforawalk/app/support/TestConfig.kt
+++ backend/app-server/src/test/kotlin/side/flab/goforawalk/app/support/TestConfig.kt
@@ -3,18 +3,27 @@ package side.flab.goforawalk.app.support
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE
+import side.flab.goforawalk.app.support.image.ImageUploader
+import side.flab.goforawalk.app.support.mock.FakeImageUploader
 
 @Profile("test")
 @Configuration
 class TestConfig(
     private val oauth2ClientProperties: OAuth2ClientProperties,
 ) {
+    @Bean
+    @Primary
+    fun testImageUploader(): ImageUploader {
+        return FakeImageUploader("fake-image-url")
+    }
+
     @Bean
     fun clientRegistrationRepository(): ClientRegistrationRepository {
         val registrations = mutableListOf<ClientRegistration>()
