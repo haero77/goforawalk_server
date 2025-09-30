70e9912328ec6d840cb2bc54140636d0bee0691e
haero77 <shineecard5@gmail.com>
2025-10-01 00:14:18 +0900

    [GW-177] docs: SQL 오류 수정

--- backend/app-server/sql/0.1.1.sql
+++ backend/app-server/sql/0.1.1.sql
@@ -12,10 +12,10 @@ create table footstep
 (
     id            bigint auto_increment,
     user_id       bigint                                    not null,
-    date          date                                      not null,
+    issue_date    date                                      not null,
     content       varchar(50),
     image_url     varchar(255)                              not null,
-    entity_status enum ('ACTIVE','DELETED') not null,
+    entity_status enum ('ACTIVE','DELETED')                 not null,
     created_at    timestamp(6) default current_timestamp(6) not null,
     updated_at    timestamp(6) default current_timestamp(6) not null,
     primary key (id)
