ae42414bfed283569ae231036ca8a871d5befaf3
haero77 <shineecard5@gmail.com>
2025-12-12 20:08:41 +0900

    [GW-210] 재로그인시 기존 리프레쉬 토큰이 삭제되지 않는 문제 수정

--- backend/app-server/src/main/kotlin/side/flab/goforawalk/app/auth/refreshtoken/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package side.flab.goforawalk.app.auth.refreshtoken
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RefreshTokenRepository : JpaRepository<RefreshToken, Long> {
-    fun findByUserId(userId: Long): RefreshToken?
-    fun deleteByUserId(userId: Long)
-}
